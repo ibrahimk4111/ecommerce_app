9c5241d64f4d1179fe81fc171fa2f38f35fa587a
ibrahim <ibrahimk4111@gmail.com>
2025-01-13 17:12:38 +0600

    ...

--- components/footer/newsletter.tsx
+++ components/footer/newsletter.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import newsletterBg from '@/public/Foot1er.png'
-import newsletterBgright from '@/public/banner-9.png.png'
 import Image from 'next/image'
 
 const Newsletter = () => {
   return (
-    <div className=' container mx-auto relative h-72 overflow-hidden '>
-      <Image src={newsletterBg} alt="newsletter bg"/>
-      <div>
-        <p>Stay home & get your daily needs from our shop</p>
+    <div className=' container mx-auto relative h-72 overflow-hidden rounded-lg '>
+      <Image src={newsletterBg} alt="newsletter bg" fill={true}/>
+      <div className=' absolute flex flex-col left-20 w-1/3 justify-center items-start gap-2 h-full '>
+        <p className=' font-bold text-2xl tracking-wide leading-10 '>Stay home & get your daily needs from our shop</p>
         <p>Start You'r Daily Shopping with Mega Mart</p>
+        {/* <Form></Form> */}
       </div>
     </div>
   )
